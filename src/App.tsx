bcd62ced5238eea21022fd5b265842a2fd8005a9
0xzay <0xzayeth@gmail.com>
2023-06-18 17:38:14 +0500

    feat: add ErrorPage

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./styles/app.scss";
 import { Main } from "./pages/Main";
 import { CreateForm } from "./pages/CreateForm";
+import { ErrorPage } from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/create",
