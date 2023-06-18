c031fb1efb4bd8bfbf5b5ca9b56e64823ab7ba80
0xzay <0xzayeth@gmail.com>
2023-06-18 17:40:20 +0500

    feat: add types

---
+++ src/@types/types.ts
@@ -0,0 +1,12 @@
+export type RequestPayloadType = {
+  nickname: string | null;
+  name: string | null;
+  surname: string | null;
+  phone: string | null;
+  email: string | null;
+  sex: string | null;
+  advantages: string[] | null;
+  radio: string | null;
+  checkbox: string[] | null;
+  about: string | null;
+};
