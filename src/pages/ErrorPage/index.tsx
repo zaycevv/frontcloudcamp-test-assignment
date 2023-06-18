b5219cd6b27ac76c27f7a996fb3055bc88175e8a
0xzay <0xzayeth@gmail.com>
2023-06-18 17:41:52 +0500

    feat: create ErrorPage with prikol

---
+++ src/pages/ErrorPage/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./ErrorPage.module.scss";
+
+export const ErrorPage = () => {
+  return (
+    <div className={styles.createForm}>
+      <div className={styles.createForm__error}>
+        <img
+          className={styles.createForm__error__secret_image}
+          src="https://i.pinimg.com/736x/63/35/92/63359215f088d3416fd8cf76a9b5a601.jpg"
+          alt=""
+        />
+        <h1>ERROR 404</h1>
+        <p>Страница не найдена, найден прикол</p>
+      </div>
+    </div>
+  );
+};
